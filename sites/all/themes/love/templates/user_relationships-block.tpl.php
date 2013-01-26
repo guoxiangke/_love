7b7f7af795d6ff944e2fa6b6e88bb07ba1f6f895
guoxiangke <xiangkeguo@gmail.com>
2013-01-26 12:06:24 +0800

    123

--- sites/all/themes/love/templates/user_relationships-block.tpl.php
+++ sites/all/themes/love/templates/user_relationships-block.tpl.php
@@ -9,16 +9,14 @@ if ($relationships) {
   $rows = array();
   $output = '';
   foreach ($relationships as $rtid => $relationship) {
-    $tt_rel_name = ur_tt("user_relationships:rtid:$rtid:name", $relationship->name);
-    $tt_rel_plural_name = ur_tt("user_relationships:rtid:$rtid:plural_name", $relationship->plural_name); 
     if ($the_other_uid == $relationship->requester_id) {
-      $rtype_heading = $relationship->is_oneway ? 
-        t("@rel_name of", array('@rel_name' => $tt_rel_name, '@rel_plural_name' => $tt_rel_plural_name)) : 
-        t("@rel_plural_name", array('@rel_name' => $tt_rel_name, '@rel_plural_name' => $tt_rel_plural_name));
+      $rtype_heading = $relationship->is_oneway ?
+        t("@rel_name of", user_relationships_type_translations($relationship)) :
+        t("@rel_plural_name", user_relationships_type_translations($relationship, TRUE));
       $relatee = $relationship->requestee;
     }
     else {
-      $rtype_heading = t("@rel_plural_name", array('@rel_name' => $tt_rel_name, '@rel_plural_name' => $tt_rel_plural_name));
+      $rtype_heading = t("@rel_plural_name", user_relationships_type_translations($relationship));
       $relatee = $relationship->requester;
     }
 
@@ -26,7 +24,6 @@ if ($relationships) {
 
     $username = theme('username', array('account' => $relatee));
     $variables['account'] = $relatee;
-
     $output  .= '<div class="recent-visit">' . "\n";
     $output .= love_layout_template_preprocess_user_picture($variables);
     $output .= ' <div class="name">' . $username . '</div>' . "\n";
