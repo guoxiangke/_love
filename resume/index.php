ff97912bcab196880efa0f6f4c1db8fad15dfcdd
guoxiangke <guo@crystal-asia.com>
2013-03-06 09:40:26 +0800

    php

---
+++ resume/index.php
@@ -0,0 +1,382 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<meta charset="utf-8">
+
+<title>求职Web开发简历-郭向科</title>
+
+<!-- Meta -->
+<meta name="description" content="A few lines about yourself">
+<meta name="keywords" content="web,developer,designer,resume" />
+<meta name="author" content="bluesky_still">
+
+<!-- CSS Stylesheet -->
+<link rel="stylesheet" type="text/css" href="css/style.css" />
+
+<!--[if lt IE 9]> 
+<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+<!-- Add jQuery library -->
+<script src="jquery-1.4.3.min.js"></script>
+
+<!-- Add fancyBox main JS and CSS files -->
+	
+
+	<script type="text/javascript" src="./fancybox/jquery.fancybox-1.3.4.pack.js"></script>
+	<link rel="stylesheet" type="text/css" href="./fancybox/jquery.fancybox-1.3.4.css" media="screen" />
+
+	
+	<script type="text/javascript">$(document).ready(function() {
+	$(".fancybox").fancybox({
+		openEffect	: 'elastic',
+		closeEffect	: 'elastic',
+		padding : 0
+	});
+});</script>
+</head>
+<body>
+
+<!-- Begin Wrapper -->
+<div id="wrapper">
+
+<!-- Begin Content Area -->
+<div id="content">
+
+<!-- Begin Header -->
+<header>
+
+<!-- Begin Contact Section -->
+<section id="contact-details">
+<div itemscope itemtype="http://schema.org/Person">
+
+<!-- Begin Profile Image Section http://my.zhaopin.com/pic/2010/11/18/D8216C11CD43402D84E8D58E2D0EA3C2.jpg -->
+<div class="header_1">
+<img src="images/picture-107-1357020565.png" itemprop="image" width="250" height="250" alt="Your Name" />
+</div>
+<!-- End Profile Image Section -->
+
+<!-- Begin Profile Information Section -->
+<div class="header_2">
+
+<h1><span itemprop="name">郭向科</span></h1>
+<h3><span itemprop="jobTitle">Web Developer / Drupaler</span></h3>
+
+<ul class="info_2">
+男，26岁，3年工作经验，执着于互联网产品开发，主攻drupal方向，热衷开源技术，接触过很多项目，有丰富的Web后端开发经验。忠诚、向上，激情，追求卓越，拥有独立思考问题解决问题能力，具有合理的知识结构、服务意识、创新意识、团队合作与管理能力。
+工作态度认真、负责，具备良好的学习能力、创新能力，善于沟通，乐于分享。
+</ul>
+
+</div>
+<!-- End Profile Information Section -->
+
+</div>
+</section>
+<!-- End Contact Section -->
+
+</header>
+<!-- End Header -->
+
+
+
+<div class="clear">&nbsp;</div>
+
+<!-- Begin Skills Section -->
+<div itemscope itemtype="http://schema.org/ItemList">
+
+<dl>
+<dt itemprop="name">专业技能:</dt>
+<dd>
+
+<section id="skills">
+<ul class="list1">
+<li itemprop="itemListElement">专业<img src="images/drupal.png"/>Drupal模块开发</li>
+<li itemprop="itemListElement">专业<img src="images/drupal.png"/>Drupal主题开发</li>
+<li itemprop="itemListElement">使用<img src="images/github.png" title="https://github.com/guoxiangke"/><a alt="https://github.com/guoxiangke"  target="_blank" href="https://github.com/guoxiangke">github</a>协同开发</li>
+<li itemprop="itemListElement">熟悉<img src="images/ps.png" title="Photoshop"/>基本操作</li>
+<li itemprop="itemListElement">熟悉LAMP开发</li>
+
+</ul>
+<ul class="list2">
+<li itemprop="itemListElement"><img src="images/centos.png" title="CenterOs"/>开发环境配置</li>
+<li itemprop="itemListElement"><img src="images/sina.png" title="SinaWeiboAPI"/>新浪微博开发平台</li>
+<li itemprop="itemListElement"><img src="http://twitter.github.com/bootstrap/assets/ico/favicon.ico" alt="" />Bootstrap</li>
+<li itemprop="itemListElement">初步<img src="images/ruby.png" height="16px" title="Rails"/>Rails</li>
+<li itemprop="itemListElement">HTML/CSS/js/jQuery</li>
+
+</ul>
+</section>
+
+</dd>
+</dl>
+
+</div>
+<!-- End Skills Section -->
+
+<div class="clear">&nbsp;</div>
+
+
+<div class="clear">&nbsp;</div>
+<!-- Begin Experience Section -->
+<dl>
+<dt>项目经验:</dt>
+
+<dd>
+
+<section id="experience">
+
+<!-- Position #1 -->
+<div itemscope itemtype="http://schema.org/Organization">
+
+<p class="bus1">0：<span itemprop="name"><img  style="width:30px;height:30px;" src="images/bcis.jpg" title="微铺街"/><a href="http://druedu.com/" target="_blank">Druedu</a> for BCIS</span></p>
+<p class="time"></p>
+
+<p itemprop="description" class="description">>
+DruEdu - Next generation education solutions for schools based on Drupal. And I worked on the druedu_qa druedu_og_user and druedu_notifications modules...
+</p>
+<p>应用截图：</p>
+<div class="fancy">
+<a href="images/bcis1.png" class="fancybox" rel="gallery1" title=""><img src="images/bcis1.png" alt="" /></a>
+<a href="images/bcis2.png" class="fancybox" rel="gallery1" title=""><img src="images/bcis2.png" alt="" /></a>
+<a href="images/bcis3.png" class="fancybox" rel="gallery1" title=""><img src="images/bcis3.png" alt="" /></a>
+<a href="images/bcis4.png" class="fancybox" rel="gallery1" title=""><img src="images/bcis4.png" alt="" /></a>
+<a href="images/bcis5.png" class="fancybox" rel="gallery1" title=""><img src="images/bcis5.png" alt="" /></a>
+</div>
+
+<!-- Position #2 -->
+<div itemscope itemtype="http://schema.org/Organization">
+
+
+<p class="bus1">1：<span itemprop="name"><a href="http://yongbuzhixi.com/" target="_blank">永不止息</a></span></p>
+
+
+<p itemprop="description" class="description">>
+永不止息是我业余时间开发的一款针对基督徒的婚恋交友网站。详情请点击进入 http://yongbuzhixi.com</p>
+<p>应用截图：</p>
+<div class="fancy">
+<a href="images/ybzx1.jpg" class="fancybox" rel="gallery1" title="登录页"><img src="images/ybzx1.jpg" alt="" /></a>
+<a href="images/ybzx2.jpg" class="fancybox" rel="gallery1" title="卖家首页"><img src="images/ybzx2.jpg" alt="" /></a>
+<a href="images/ybzx3.jpg" class="fancybox" rel="gallery1" title="店铺街"><img src="images/ybzx3.jpg" alt="" /></a>
+<a href="images/ybzx4.jpg" class="fancybox" rel="gallery1" title="广场"><img src="images/ybzx4.jpg" alt="" /></a>
+<a href="images/ybzx5.jpg" class="fancybox" rel="gallery1" title="私信页面"><img src="images/ybzx5.jpg" alt="" /></a>
+<a href="images/ybzx6.jpg" class="fancybox" rel="gallery1" title="点评页面"><img src="images/ybzx6.jpg" alt="" /></a>
+<a href="images/ybzx7.jpg" class="fancybox" rel="gallery1" title="点评页面"><img src="images/ybzx7.jpg" alt="" /></a>
+</div>
+</div>
+
+<p class="bus1">3：<span itemprop="name"><a href="http://test.weipujie.com/" target="_blank"><img src="images/vpj.png" title="微铺街"/>微铺街</a></span></p>
+<p class="time"></p>
+
+<p itemprop="description" class="description">>
+微铺街是一款基于微博分享某一领域的信息聚合的互联网产品。<!--定位于爱分享的购物时尚女性，想与买家更多互动及网络营销的淘宝卖家。基于评论的第三方点评平台，解决现阶段淘宝卖家与买家信任危机，同时配有android移动客户端程序，让买家随时随地分享及了解所关注店铺最新活动信息。-->在该项目中，本人担当主要技术开发人员（其实就我一人开发，后期聘请一些前端和设计），同时接触产品设计，原型讨论，等产品研发的整个流程。主要微博API结合drupal开发，主要业务逻辑模块实现，用户关系定制开发，与android手机客户端交互服务模块开发。 
+</p>
+<p>应用截图：</p>
+<div class="fancy">
+<a href="images/vpj1.png" class="fancybox" rel="gallery1" title="买家首页"><img src="images/vpj1.png" alt="" /></a>
+<a href="images/vpj6.png" class="fancybox" rel="gallery1" title="卖家首页"><img src="images/vpj6.png" alt="" /></a>
+<a href="images/vpj2.png" class="fancybox" rel="gallery1" title="店铺街"><img src="images/vpj2.png" alt="" /></a>
+<a href="images/vpj3.png" class="fancybox" rel="gallery1" title="广场"><img src="images/vpj3.png" alt="" /></a>
+<a href="images/vpj4.png" class="fancybox" rel="gallery1" title="私信页面"><img src="images/vpj4.png" alt="" /></a>
+<a href="images/vpj5.png" class="fancybox" rel="gallery1" title="点评页面"><img src="images/vpj5.png" alt="" /></a>
+</div>
+
+</div>
+<!-- Position #1 -->
+<div itemscope itemtype="http://schema.org/Organization">
+
+<p class="bus1">3：<span itemprop="name"><a href="http://cc.agapebarn.com/" target="_blank"><img src="images/cc2.png" title="爱心粮仓"/>爱心粮仓</a></span></p>
+<p class="time"></p>
+
+<p itemprop="description" class="description">>
+爱心粮仓是基于Drupal OpenChurch Distribution制作的一个小站，同样，非正式项目，没有设计，本人8月份业余时间帮助NGO免费制作的。
+</p>
+
+</div>
+
+<!-- Position #2 -->
+<div itemscope itemtype="http://schema.org/Organization">
+
+
+<p class="bus1">4：<span itemprop="name"><a href="http://bibles.sinaapp.com/" target="_blank"><img src="http://onepagelove.com/wp-content/themes/oplv3/img/heart.png" alt="" />圣经在线</a></span></p>
+
+
+<p itemprop="description" class="description">>
+凭兴趣开发的简单drupal+SAE应用</p>
+
+</div>
+ <!-- Position #2 -->
+<div itemscope itemtype="http://schema.org/Organization">
+
+
+<p class="bus1">5：SAE应用开发：<span itemprop="name"><a href="http://livingwater.sinaapp.com/" target="_blank">SAE短信平台</a>（需登录）</span></p>
+
+
+<p itemprop="description" class="description" >>
+使用SAE 短信API，基于drupal开发批量发送短信平台。</p>
+
+</div>
+
+</section>
+
+</dd>
+</dl>
+<!-- End Experience Section -->
+
+<div class="clear">&nbsp;</div>
+<!-- Begin Publications Section -->
+<dl>
+<dt>技术领域:</dt>
+
+<dd>
+
+<section id="publications">
+
+<div itemscope itemtype="http://schema.org/Article"><span itemprop="name">
+<img src="images/markdown.ico" alt="" />Markdown
+<img src="images/git.ico" alt="" />Git
+<img src="http://forum.ubuntu.org.cn/favicon.ico" alt="" />Ubuntu
+<img src="http://switchy.samabox.com/images/icon-128.png" alt="switchy" width="32px"/>switchy!
+<img src="http://developer.android.com/assets/images/dac_logo.png" alt="" /><br/>
+<span id="sae"><a href="http://sae.sina.com.cn" target="_blank" alt="SAE">SAE</a></span>
+<img src="http://bkarak.wizhut.com/blog/wp-content/uploads/2012/01/app_engine-64.png" alt="switchy"/>
+<img src="http://symfony.com/images/common/logo/logo_symfony_header.png" alt="switchy"/>
+<img src="http://www.phonegap.cn/wp-content/uploads/2011/07/phonegap2.png" alt="" />
+<p>
+<li itemprop="itemListElement">godaddy、bluehost、万网、西部数码等vps生产环境配置</li>
+<li itemprop="itemListElement">Win2003服务器维护配置</li>
+<li itemprop="itemListElement">SQL server</li>
+<li itemprop="itemListElement">机房管理、网络维护</li></p>
+<style type="text/css">
+#sae{
+font-size: 30px;
+line-height: 36px;
+display:block;
+}
+#sae a{display: block;
+float: left;
+width: 96px;
+height: 62px;
+text-indent: -999em;
+background: url('http://sae.sina.com.cn/static/image/logo_new.gif') left top no-repeat;
+background-position: 0 -60px;
+}
+.fancy img{
+	width:150px;
+}
+.description{
+	font-size:13px;
+}
+</style>
+
+</div>
+
+</section>
+
+</dd>
+</dl>
+<!-- End Publications Section -->
+<!-- <div class="clear">&nbsp;</div> -->
+<!-- Begin Publications Section -->
+<!-- <dl>
+<dt>开源贡献:</dt>
+
+<dd>
+
+<section id="publications">
+
+<div itemscope itemtype="http://schema.org/Article"><span itemprop="name">
+翻译 The Definitive Guide to Drupal 7.
+
+</div>
+
+</section>
+
+</dd>
+</dl> -->
+<!-- End Publications Section -->
+<!-- 
+<div class="clear">&nbsp;</div> -->
+<!-- Begin Publications Section -->
+
+<!-- Begin Education Section -->
+<!-- <dl>
+<dt>早期项目:</dt>
+<dd>
+
+<section id="education">
+<div itemscope itemtype="http://schema.org/EducationalOrganization">
+<pre>2011/06 -- 2011/07：丰田金融学院论坛基于drupal系统的定制开发
+2011/05 -- 2011/06：兴家牧业REP维护drupal菜单系统，中英文解决方案，文件丢失调试
+2011/04 -- 2011/05：丰田金融e-learning需求开发 基于moodle选课系统开发和日志系统扩展
+2011/03 -- 2011/04：第四届方圆工程演讲赛 界面调试，投票功能开发，其他需求开发 
+2011/03 -- 2011/04：骨松病例征集  COA 子站 
+2011/03 -- 2011/04：协和医院发展论坛 架构构建 功能完善
+2011/03 -- 2011/04：解放军总医院关节中心 网站搭建 功能实现
+2011/01 -- 2011/03：糖医网 E-Learning课件制作，2010/12 -- 20101/12：凝聚力
+</pre>
+
+</div>
+</section>
+
+</dd>
+</dl> -->
+<!-- End Education Section -->
+
+<div class="clear">&nbsp;</div>
+
+<!-- Begin Profile Section -->
+<dl>
+<dt>求职意向:</dt>
+<dd>
+
+<section class="summary">网站开发相关，主要是编写代码，产品或交互也行，喜欢用户交互与研究，不过经验不足，喜欢开放的环境，活跃的团队，期待月薪税前<b>8k～11k</b>，非诚勿扰。到岗时间，随时。
+</section>
+
+</dd>
+</dl>
+<!-- End Profile Section -->
+
+<div class="clear">&nbsp;</div>
+<!-- Begin Profile Section -->
+<dl>
+<dt>联系我:</dt>
+<dd>
+
+<section class="summary">
+	 <div itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
+		<ul class="info_1">
+			<li class="phone"><span itemprop="telephone">(+86) 137-1658-7629</span></li>
+			<li class="email">xiangkeguo#gmail.com</li>
+
+		</ul>
+	</div>
+
+<img  style="CURSOR: pointer" onclick="javascript:window.open('http://b.qq.com/webc.htm?new=0&sid=474054236&o=天空蔚蓝&q=7', '_blank', 'height=502, width=644,toolbar=no,scrollbars=no,menubar=no,status=no');"  border="0" SRC=http://wpa.qq.com/pa?p=1:474054236:7 alt="选我！">474054236
+<a rel="me" itemprop="url" href="http://drupal.stackexchange.com/users/10257/bluesky-still?tab=questions" title="Follow Me on Twitter">My questions on <img src="http://cdn.sstatic.net/drupal/img/favicon.ico" title="drupal.stackexchange.com"/></a>
+<a alt="https://github.com/guoxiangke"  target="_blank" href="https://github.com/guoxiangke">My github<img src="images/github.png" title="https://github.com/guoxiangke"/></a>
+<iframe width="230" height="24" frameborder="0" allowtransparency="true" marginwidth="0" marginheight="0" scrolling="no" border="0" src="http://widget.weibo.com/relationship/followbutton.php?language=zh_cn&width=230&height=24&uid=1801497981&style=3&btn=red&dpc=1"></iframe>
+
+<!-- End PostalAddress Schema -->
+
+
+</section>
+
+</dd>
+</dl>
+<!-- End Profile Section -->
+
+
+</div>
+</section>
+<!-- End Contact Section -->
+
+
+
+</div>
+<!-- End Content -->
+
+</div>
+<!-- End Wrapper -->
+
+</body>
+</html>
