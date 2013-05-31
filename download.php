16feaa082143fbf8a07c74935cf2600442154792
xiangkeguo <xiangkeguo@gmail.com>
2013-05-31 17:25:43 +0800

    download &

---
+++ download.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+<head>
+<title>下载列表(周<?php
+$today=mktime(0,0,0,date('m'),date('d'),date('Y'));
+$get_date=$_GET["date"]?mktime(0,0,0,date('m',$_GET["date"]),date('d',$_GET["date"]),date('Y',$_GET["date"])):$today;
+$date= $_GET["date"]?date("ymd",$get_date):date("ymd");
+echo $showtime=date("w");
+?>节目)</title>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<style type="text/css">
+	.program{text-align:left;}
+	.program div{padding-left:10px;width:80px;}
+</style>
+</head>
+<body>
+
+
+<div align=center><br/><b>下载列表(周<?php
+echo $showtime=date("w",$get_date);
+?>节目)</b><br/>
+<br/><br/>
+<?php
+$program=array(
+				'ws'=>'长夜的牵引',
+				'bv'=>'灵命日粮',
+				'ee'=>'拥抱每一天',
+				'se'=>'恋爱季节',
+				'hg'=>'欢乐卡洽碰',
+				'bc'=>'书香园地',
+				'ds'=>'晨曦讲座',
+				'gv'=>'生活无国界',
+				'cc'=>'空中辅导',
+				'up'=>'亲情不断电',
+				'bf'=>'幸福满家园',
+				'eg'=>'英语世界',
+				);
+foreach($program as $key=>$value){
+	$mp3 = array('se');
+	$extend = '.wma';
+	if(in_array($key, $mp3)) {
+		$extend = '.mp3';
+	}
+	echo "<div class='program'>
+	<span> $value</span>
+	<span><a href='http://liangyou.nissigz.com/media/$key/$key".$date.$extend."'>线路一</a> </span>
+	<span><a href='http://audio.liangyou.net/files/media/program_live/$key/$key".$date.$extend."'>线路二</a></span>
+	<span><a href='http://audio2.liangyou.net/files/media/program_live/$key/$key".$date.$extend."'>线路三</a></span>
+	<span><a href='http://audio.liangyou.net/download.php?t=2&c=$key&f=$key".$date.$extend."'>线路4</a></span>
+	</div><br/>";
+}
+?>
+<div><?php $yes=$get_date-3600*24;?><?php
+if( time()-$yes>3600*24*7){
+}else{
+echo '<a href="download_list.php?date='.$yes.'">前一天的节目</a>';
+}
+?>
+
+注：书香园地 周一-周五
+</body>
+</html>
--- sites/all/themes/love/templates/page--welcome.tpl.php
+++ sites/all/themes/love/templates/page--welcome.tpl.php
@@ -57,11 +57,11 @@
         <div class="site-des">
           <p>&nbsp;&nbsp;</p>
           <ul>
-            <li>【永不止息】只接受熟人间邮箱邀请注册，请为您邀请的肢体祷告，您在熟人之间互相介绍时，应慎重，严谨，并为之代祷。<a href="#" rel="tooltip" data-placement="right" title="【永不止息】是@bluesky_still透过对当前教会单身肢体需求的看见，业余时间开发的针对主内肢体婚恋社交网站，通过简洁的关系建立渠道，熟人的推荐，舆论监督与指导，为肢体提供一个便捷互相了解、信实互动的社交平台。">成长历程</a></li>
+            <li>【永不止息】只接受熟人间邮箱邀请注册，请为您邀请的肢体祷告，您在熟人之间互相介绍时，应慎重，严谨，并为之代祷。</li>
             
-            <li>为<a href="#" rel="tooltip" title="请为本站营造良好的社交环境。<br/>为教会单身守望。">本站代祷</a> ，<a href="#" rel="tooltip" title="如果您觉得打开速度慢，那么您可以在网站服务器升级上有分">支持奉献</a>，<a href="http://blog.liangyou.net/lianai/" rel="tooltip" title="打开速度慢，这个建议就不用提了^_^">意见建议</a></li>
+            <li>为<a href="#" rel="tooltip" title="请为本站营造良好的社交环境。<br/>为教会单身守望。">本站代祷</a> ，<a href="https://me.alipay.com/guoxiangke" target="_blank" rel="tooltip" title="如果您觉得打开速度慢，那么您可以在网站服务器升级上有分">支持奉献</a>，<a href="#" rel="tooltip" title="打开速度慢，这个建议就不用提了^_^">意见建议</a>，<a href="#" rel="tooltip" data-placement="top" title="【永不止息】是@bluesky_still透过对当前教会单身肢体需求的看见，业余时间开发的针对主内肢体婚恋社交网站，通过简洁的关系建立渠道，熟人的推荐，舆论监督与指导，为肢体提供一个便捷互相了解、信实互动的社交平台。">成长历程</a></li>
             <!--li><a href="http://blog.liangyou.net/lianai/"  rel="tooltip" title="如果您还未收到邀请，听听这个节目，预备自己成为理想的另一半吧！"><img src="<?php print path_to_theme(); ?>/lianai.jpg" alt=""></a><a href="#" rel="tooltip" title="打开速度慢，这个建议就不用提了^_^"></a></li-->
-            <li><embed type="application/x-shockwave-flash" width="240" height="24" src="<?php echo 'http://'.$_SERVER['HTTP_HOST'].'/'.drupal_get_path('module','love')?>/player.swf?soundFile=http://liangyou.nissigz.com/media/se/se<?php echo date("ymd");?>.mp3&amp;bg=0xCDDFF3&amp;leftbg=0x357DCE&amp;lefticon=0xF2F2F2&amp;rightbg=0x357DCE&amp;rightbghover=0x4499EE&amp;righticon=0xF2F2F2&amp;righticonhover=0xFFFFFF&amp;text=0x357DCE&amp;slider=0x357DCE&amp;track=0xFFFFFF&amp;border=0xFFFFFF&amp;loader=0x8EC2F4&amp;autostart=no&amp;loop=no" wmode="transparent" quality="high"><a href="#"  title="未收到邀请？听听这个节目，预备自己成为理想的另一半吧！">今日广播</a>
+            <li><a  href="http://blog.liangyou.net/lianai/"  rel="tooltip"  title="未收到邀请？听听这个节目，预备自己成为理想的另一半吧！">今日广播</a><embed type="application/x-shockwave-flash" width="240" height="24" src="<?php echo 'http://'.$_SERVER['HTTP_HOST'].'/'.drupal_get_path('module','love')?>/player.swf?soundFile=http://liangyou.nissigz.com/media/se/se<?php echo date("ymd");?>.mp3&amp;bg=0xCDDFF3&amp;leftbg=0x357DCE&amp;lefticon=0xF2F2F2&amp;rightbg=0x357DCE&amp;rightbghover=0x4499EE&amp;righticon=0xF2F2F2&amp;righticonhover=0xFFFFFF&amp;text=0x357DCE&amp;slider=0x357DCE&amp;track=0xFFFFFF&amp;border=0xFFFFFF&amp;loader=0x8EC2F4&amp;autostart=no&amp;loop=no" wmode="transparent" quality="high">
 </li>          </ul>
         </div>
       </div>
