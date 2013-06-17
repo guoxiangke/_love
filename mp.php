d4d1d86c9d17f99ea74f50ddfece455d4d3e7c4e
xiangkeguo <xiangkeguo@gmail.com>
2013-06-18 01:11:30 +0800

    add mp-receive node type debug1

--- mp.php
+++ mp.php
@@ -65,14 +65,14 @@ class wechatCallbackapiTest
 
                     $node = new stdClass();
                     $node->title = $postObj->MsgId;
-                    $node->type = 'mp-receive';
+                    $node->type = 'mp_receive';
                     node_object_prepare($node); // Sets some defaults. Invokes hook_prepare() and hook_node_prepare().
                     $node->language = LANGUAGE_NONE; // Or e.g. 'en' if locale is enabled
                     $node->uid = 1;
                     $node->status = 1; //(1 or 0): published or not
                     $node->promote = 0; //(1 or 0): promoted to front page
                     $node->comment = 0; // 0 = comments disabled, 1 = read only, 2 = read/write
-
+                    $node->path['pathauto'] = false;
                     $node->body[$node->language][0]['value']   = $postObj->Content . print_r($postObj,true);
                     $node->body[$node->language][0]['summary'] = text_summary($postObj->Content);
                     $node->body[$node->language][0]['format']  = 'filtered_html';
