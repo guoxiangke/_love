d0bbfc76a6d542b92aaaff578b8343066420bbff
xiangkeguo@gmail.com <xiangkeguo@gmail.com>
2013-11-08 11:18:11 +0800

    comment--node-topic.tpl.php

---
+++ sites/all/themes/love/templates/comment--node-topic.tpl.php
@@ -0,0 +1,24 @@
+<article class="<?php print $classes . ' ' . $zebra; ?>"<?php print $attributes; ?>>
+  
+  <header>
+    <p class="submitted">
+      <?php print $picture; ?>
+      <?php print $submitted; ?>
+      <?php print $permalink; ?>
+    </p>
+  <?php
+    // We hide the comments and links now so that we can render them later.
+    hide($content['links']);
+    print render($content);
+  ?>    <?php if ($new): ?>
+          <mark class="new"><?php print $new; ?></mark>
+        <?php endif; ?>
+
+  <?php if ($signature): ?>
+    <footer class="user-signature clearfix">
+      <?php print $signature; ?>
+    </footer>
+  <?php endif; ?>
+
+  <?php print render($content['links']) ?>
+</article> <!-- /.comment -->
