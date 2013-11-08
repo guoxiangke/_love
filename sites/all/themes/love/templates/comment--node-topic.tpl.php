59e57269c6d3a6b5060280ffe6a6e8825b11a5bc
xiangkeguo@gmail.com <xiangkeguo@gmail.com>
2013-11-08 19:28:35 +0800

    update css

--- sites/all/themes/love/templates/comment--node-topic.tpl.php
+++ sites/all/themes/love/templates/comment--node-topic.tpl.php
@@ -4,27 +4,15 @@
     <div class="submitted">
       <?php print $picture; ?>
       <?php print $submitted; ?>
-      <?php print $permalink; ?>
+      <?php //print $permalink; ?>
     </div>
 
-    <?php print render($title_prefix); ?>
-    <?php if ($title): ?>
-      <h3<?php print $title_attributes; ?>>
-        <?php print $title; ?>
-        <?php if ($new): ?>
-          <mark class="new"><?php print $new; ?></mark>
-        <?php endif; ?>
-      </h3>
-    <?php elseif ($new): ?>
-      <mark class="new"><?php print $new; ?></mark>
-    <?php endif; ?>
-    <?php print render($title_suffix); ?>
   </header>
 
   <?php
     // We hide the comments and links now so that we can render them later.
     hide($content['links']);
-    // print render($content);
+    print render($content);
   ?>
 
   <?php if ($signature): ?>
