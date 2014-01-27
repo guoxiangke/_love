2fb951a4bb38bbdd81d5a3bd8eb88c7f42c3ff5e
dale-ubuntu <guoxiangke@ylnwt.net>
2014-01-27 16:58:39 +0800

    0325diary

--- sites/all/themes/b3/template.php
+++ sites/all/themes/b3/template.php
@@ -4,3 +4,9 @@
  * @file
  * template.php
  */
+function b3_preprocess_page(&$vars, $hook) {
+  if (isset($vars['node'])) {
+    // If the node type is "blog_madness" the template suggestion will be "page--blog-madness.tpl.php".
+    $vars['theme_hook_suggestions'][] = 'page__'. $vars['node']->type;
+  }
+}
