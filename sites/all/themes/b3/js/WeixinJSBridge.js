a7e4a0b3aa7b4ebd9deee8a0ecdc2466ee52bca7
dale-ubuntu <guoxiangke@ylnwt.net>
2014-02-20 11:59:05 +0800

    wxgz

--- sites/all/themes/b3/js/WeixinJSBridge.js
+++ sites/all/themes/b3/js/WeixinJSBridge.js
@@ -1,6 +1,6 @@
 document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() {
 	WeixinJSBridge.call('hideOptionMenu');
 	(function ($){
-		$('body').append('<a href="weixin://contacts/profile/love_yongbuzhixi" title="关注微信" style="position: fixed;bottom: 0px;right: 0px;"><img src="http://wx.dev.camplus.hk/sites/default/files/styles/canvas30/public/pictures/picture-172-1356487827.jpg"/></a>');
+		$('body').append('<a href="weixin://contacts/profile/love_yongbuzhixi" title="关注微信" style="position: fixed;bottom: 0px;right: 0px;"><img src="http://wx.dev.camplus.hk/sites/default/files/styles/user_profile_sc_100/public/pictures/picture-172-1356487827.jpg" style="width:40px;"/></a>');
 	}(jQuery));
 });
