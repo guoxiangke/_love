a29eb679f494dea0a277d96c7d5d6428e5f23c66
dale-ubuntu <guoxiangke@ylnwt.net>
2014-02-22 11:45:27 +0800

    gh_b945c807e7ec

--- sites/all/themes/b3/js/WeixinJSBridge.js
+++ sites/all/themes/b3/js/WeixinJSBridge.js
@@ -1,6 +1,6 @@
 document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() {
 	WeixinJSBridge.call('hideOptionMenu');
 	(function ($){
-		$('body').append('<a href="weixin://contacts/profile/love_yongbuzhixi" title="关注微信" style="position: fixed;bottom: 0px;right: 0px;">关注<br/><img src="http://wx.dev.camplus.hk/sites/default/files/styles/user_profile_sc_100/public/pictures/picture-172-1356487827.jpg" style="width:40px;"/></a>');
+		$('body').append('<a href="weixin://contacts/profile/gh_b945c807e7ec" title="关注微信" style="position: fixed;bottom: 0px;right: 0px;">关注<br/><img src="http://wx.dev.camplus.hk/sites/default/files/styles/user_profile_sc_100/public/pictures/picture-172-1356487827.jpg" class="img-circle" style="width:50px;"/></a>');
 	}(jQuery));
 });
