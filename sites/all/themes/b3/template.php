9e9e2c57b037e7c8f3a9beb9be9598cf31f9e122
dale-ubuntu <guoxiangke@ylnwt.net>
2014-02-25 14:13:45 +0800

    add themes

--- sites/all/themes/b3/template.php
+++ sites/all/themes/b3/template.php
@@ -10,3 +10,46 @@ function b3_preprocess_page(&$vars, $hook) {
     $vars['theme_hook_suggestions'][] = 'page__'. $vars['node']->type;
   }
 }
+/**
+ * Overrides theme_menu_local_tasks().
+ */
+function b3_menu_local_tasks(&$variables) {
+  $output = '';
+
+  if (!empty($variables['primary'])) {
+    $variables['primary']['#prefix'] = '<h2 class="element-invisible">' . t('Primary tabs') . '</h2>';
+    $variables['primary']['#prefix'] .= '<div><ul class="tabs--secondary pagination pagination-large">';
+    $variables['primary']['#suffix'] = '</ul><div>';
+    $output .= drupal_render($variables['primary']);
+  }
+
+  if (!empty($variables['secondary'])) {
+    $variables['secondary']['#prefix'] = '<h2 class="element-invisible">' . t('Secondary tabs') . '</h2>';
+    $variables['secondary']['#prefix'] .= '<ul class="tabs--secondary pagination pagination-sm">';
+    $variables['secondary']['#suffix'] = '</ul>';
+    $output .= drupal_render($variables['secondary']);
+  }
+
+  return $output;
+}
+/**
+ * Overrides theme_breadcrumb().
+ *
+ * Print breadcrumbs as an ordered list.
+ */
+function b3_breadcrumb($variables) {
+  $output = '';
+  $breadcrumb = $variables['breadcrumb'];
+  // Determine if we are to display the breadcrumb.
+  $bootstrap_breadcrumb = theme_get_setting('bootstrap_breadcrumb');
+  if (($bootstrap_breadcrumb == 1 || ($bootstrap_breadcrumb == 2 && arg(0) == 'admin')) && !empty($breadcrumb)) {
+    $output = theme('item_list', array(
+      'attributes' => array(
+        'class' => array('breadcrumb'),
+      ),
+      'items' => $breadcrumb,
+      'type' => 'ol',
+    ));
+  }
+  return $output;
+}
