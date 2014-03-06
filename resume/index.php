5896d3e0573eecdb8f9f090884c73fd1d7e66861
xiangkeguo@gmail.com <xiangkeguo@gmail.com>
2014-03-07 06:12:47 +0800

    resume

--- resume/index.php
+++ resume/index.php
@@ -18,14 +18,12 @@
 <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 <![endif]-->
 <!-- Add jQuery library -->
-<script src="jquery-1.4.3.min.js"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
 
 <!-- Add fancyBox main JS and CSS files -->
-	
 
 	<script type="text/javascript" src="./fancybox/jquery.fancybox-1.3.4.pack.js"></script>
 	<link rel="stylesheet" type="text/css" href="./fancybox/jquery.fancybox-1.3.4.css" media="screen" />
-
 	
 	<script type="text/javascript">$(document).ready(function() {
 	$(".fancybox").fancybox({
@@ -63,8 +61,8 @@
 <h3><span itemprop="jobTitle">Web Developer / Drupaler</span></h3>
 
 <ul class="info_2">
-男，26岁，3年工作经验，执着于互联网产品开发，主攻drupal方向，热衷开源技术，接触过很多项目，有丰富的Web后端开发经验。忠诚、向上，激情，追求卓越，拥有独立思考问题解决问题能力，具有合理的知识结构、服务意识、创新意识、团队合作与管理能力。
-工作态度认真、负责，具备良好的学习能力、创新能力，善于沟通，乐于分享。
+男，27岁，4年工作经验，执着于互联网产品开发，主攻drupal方向，热衷开源技术，接触过很多项目，有丰富的Web后端开发经验。忠诚、向上，激情，追求卓越，拥有独立思考问题解决问题能力，具有合理的知识结构、服务意识、创新意识、团队合作与管理能力。
+工作态度认真、负责，具备良好的学习能力、创新能力，善于沟通，乐于分享，现任一家互联网公司Drupal技术总监。
 </ul>
 
 </div>
@@ -93,14 +91,14 @@
 <li itemprop="itemListElement">专业<img src="images/drupal.png"/>Drupal模块开发</li>
 <li itemprop="itemListElement">专业<img src="images/drupal.png"/>Drupal主题开发</li>
 <li itemprop="itemListElement">使用<img src="images/github.png" title="https://github.com/guoxiangke"/><a alt="https://github.com/guoxiangke"  target="_blank" href="https://github.com/guoxiangke">github</a>协同开发</li>
-<li itemprop="itemListElement">熟悉<img src="images/ps.png" title="Photoshop"/>基本操作</li>
-<li itemprop="itemListElement">熟悉LAMP开发</li>
+<li itemprop="itemListElement">熟悉 <img src="http://www.golaravel.com/assets/img/favicon.ico" title="laravel"/>Laravel</li>
+<li itemprop="itemListElement">熟悉MAMP/LAMP开发</li>
 
 </ul>
 <ul class="list2">
 <li itemprop="itemListElement"><img src="images/centos.png" title="CenterOs"/>开发环境配置</li>
 <li itemprop="itemListElement"><img src="images/sina.png" title="SinaWeiboAPI"/>新浪微博开发平台</li>
-<li itemprop="itemListElement"><img src="http://twitter.github.com/bootstrap/assets/ico/favicon.ico" alt="" />Bootstrap</li>
+<li itemprop="itemListElement"><img  width="15px" src="http://www.bootcss.com/assets/ico/favicon.png" alt="" />Bootstrap 3</li>
 <li itemprop="itemListElement">初步<img src="images/ruby.png" height="16px" title="Rails"/>Rails</li>
 <li itemprop="itemListElement">HTML/CSS/js/jQuery</li>
 
@@ -119,40 +117,24 @@
 <div class="clear">&nbsp;</div>
 <!-- Begin Experience Section -->
 <dl>
-<dt>项目经验: </dt>
+<dt>项目经验:</dt>
 
 <dd>
 
 <section id="experience">
 
-<!-- Position #1 -->
-<div itemscope itemtype="http://schema.org/Organization">
-
-<p class="bus1">0：<span itemprop="name"><img  style="width:30px;height:30px;" src="images/bcis.jpg" title="微铺街"/><a href="http://druedu.com/" target="_blank">Druedu</a> for BCIS</span></p>
-<p class="time"></p>
-
-<p itemprop="description" class="description">>
-DruEdu - Next generation education solutions for schools based on Drupal. And I worked on the druedu_qa druedu_og_user and druedu_notifications modules...
-</p>
-<p>应用截图：</p>
-<div class="fancy">
-<a href="images/bcis1.png" class="fancybox" rel="gallery1" title=""><img src="images/bcis1.png" alt="" /></a>
-<a href="images/bcis2.png" class="fancybox" rel="gallery1" title=""><img src="images/bcis2.png" alt="" /></a>
-<a href="images/bcis3.png" class="fancybox" rel="gallery1" title=""><img src="images/bcis3.png" alt="" /></a>
-<a href="images/bcis4.png" class="fancybox" rel="gallery1" title=""><img src="images/bcis4.png" alt="" /></a>
-<a href="images/bcis5.png" class="fancybox" rel="gallery1" title=""><img src="images/bcis5.png" alt="" /></a>
-</div>
-
 <!-- Position #2 -->
 <div itemscope itemtype="http://schema.org/Organization">
 
 
-<p class="bus1">1：<span itemprop="name"><a href="http://yongbuzhixi.com/" target="_blank">永不止息</a></span></p>
+<p class="bus1">1：<span itemprop="name">
+	<img  style="width:35px;" src="http://wx.dev.camplus.hk/sites/default/files/styles/canvas100/public/pictures/picture-172-1356487827.jpg" title=""/>
+	永不止息</span></p>
 
 
 <p itemprop="description" class="description">>
-永不止息是我业余时间开发的一款针对基督徒的婚恋交友网站。详情请点击进入 http://yongbuzhixi.com</p>
-<p>应用截图：</p>
+<a href="http://yongbuzhixi.com/" target="_blank">永不止息</a> 是我业余时间开发的一款针对基督徒的婚恋交友网站。详情请点击进入 <a href="http://yongbuzhixi.com/" target="_blank">http://yongbuzhixi.com</a></p>
+<p class="screenshot">应用截图：</p>
 <div class="fancy">
 <a href="images/ybzx1.jpg" class="fancybox" rel="gallery1" title="登录页"><img src="images/ybzx1.jpg" alt="" /></a>
 <a href="images/ybzx2.jpg" class="fancybox" rel="gallery1" title="卖家首页"><img src="images/ybzx2.jpg" alt="" /></a>
@@ -164,13 +146,31 @@ DruEdu - Next generation education solutions for schools based on Drupal. And I 
 </div>
 </div>
 
-<p class="bus1">3：<span itemprop="name"><a href="http://test.weipujie.com/" target="_blank"><img src="images/vpj.png" title="微铺街"/>微铺街</a></span></p>
+<!-- Position #1 -->
+<div itemscope itemtype="http://schema.org/Organization">
+
+<p class="bus1">2：<span itemprop="name"><img  style="width:30px;height:30px;" src="images/bcis.jpg" title=""/>Druedu for BCIS</span></p>
 <p class="time"></p>
 
 <p itemprop="description" class="description">>
-微铺街是一款基于微博分享某一领域的信息聚合的互联网产品。<!--定位于爱分享的购物时尚女性，想与买家更多互动及网络营销的淘宝卖家。基于评论的第三方点评平台，解决现阶段淘宝卖家与买家信任危机，同时配有android移动客户端程序，让买家随时随地分享及了解所关注店铺最新活动信息。-->在该项目中，本人担当主要技术开发人员（其实就我一人开发，后期聘请一些前端和设计），同时接触产品设计，原型讨论，等产品研发的整个流程。主要微博API结合drupal开发，主要业务逻辑模块实现，用户关系定制开发，与android手机客户端交互服务模块开发。 
+<a href="###" target="_blank">DruEdu</a> - Next generation education solutions for schools based on Drupal. And I worked on the druedu_qa druedu_og_user and druedu_notifications modules...
 </p>
-<p>应用截图：</p>
+<p class="screenshot">应用截图：</p>
+<div class="fancy">
+<a href="images/bcis1.png" class="fancybox" rel="gallery1" title=""><img src="images/bcis1.png" alt="" /></a>
+<a href="images/bcis2.png" class="fancybox" rel="gallery1" title=""><img src="images/bcis2.png" alt="" /></a>
+<a href="images/bcis3.png" class="fancybox" rel="gallery1" title=""><img src="images/bcis3.png" alt="" /></a>
+<a href="images/bcis4.png" class="fancybox" rel="gallery1" title=""><img src="images/bcis4.png" alt="" /></a>
+<a href="images/bcis5.png" class="fancybox" rel="gallery1" title=""><img src="images/bcis5.png" alt="" /></a>
+</div>
+
+<p class="bus1">3：<span itemprop="name"><img src="images/vpj.png" title="微铺街"/>微铺街</a></span></p>
+<p class="time"></p>
+
+<p itemprop="description" class="description">>
+<a href="http://test.weipujie.com/" target="_blank">微铺街</a>是一款基于微博分享某一领域的信息聚合的互联网产品。<!--定位于爱分享的购物时尚女性，想与买家更多互动及网络营销的淘宝卖家。基于评论的第三方点评平台，解决现阶段淘宝卖家与买家信任危机，同时配有android移动客户端程序，让买家随时随地分享及了解所关注店铺最新活动信息。-->在该项目中，本人担当主要技术开发人员（其实就我一人开发，后期聘请一些前端和设计），同时接触产品设计，原型讨论，等产品研发的整个流程。主要微博API结合drupal开发，主要业务逻辑模块实现，用户关系定制开发，与android手机客户端交互服务模块开发。 
+</p>
+<p class="screenshot">应用截图：</p>
 <div class="fancy">
 <a href="images/vpj1.png" class="fancybox" rel="gallery1" title="买家首页"><img src="images/vpj1.png" alt="" /></a>
 <a href="images/vpj6.png" class="fancybox" rel="gallery1" title="卖家首页"><img src="images/vpj6.png" alt="" /></a>
@@ -184,11 +184,11 @@ DruEdu - Next generation education solutions for schools based on Drupal. And I 
 <!-- Position #1 -->
 <div itemscope itemtype="http://schema.org/Organization">
 
-<p class="bus1">3：<span itemprop="name"><a href="http://cc.agapebarn.com/" target="_blank"><img src="images/cc2.png" title="爱心粮仓"/>爱心粮仓</a></span></p>
+<p class="bus1">4：<span itemprop="name"><img src="images/cc2.png" title="爱心粮仓"/>爱心粮仓</span></p>
 <p class="time"></p>
 
 <p itemprop="description" class="description">>
-爱心粮仓是基于Drupal OpenChurch Distribution制作的一个小站，同样，非正式项目，没有设计，本人8月份业余时间帮助NGO免费制作的。
+<a href="###" target="_blank">爱心粮仓</a>是基于Drupal OpenChurch Distribution制作的一个小站，同样，非正式项目，没有设计，业余时间帮助NGO免费制作的。
 </p>
 
 </div>
@@ -197,22 +197,22 @@ DruEdu - Next generation education solutions for schools based on Drupal. And I 
 <div itemscope itemtype="http://schema.org/Organization">
 
 
-<p class="bus1">4：<span itemprop="name"><a href="http://bibles.sinaapp.com/" target="_blank"><img src="http://onepagelove.com/wp-content/themes/oplv3/img/heart.png" alt="" />圣经在线</a></span></p>
+<p class="bus1">5：<span itemprop="name"><img src="http://onepagelove.com/wp-content/themes/oplv3/img/heart.png" alt="" />圣经在线</span></p>
 
 
 <p itemprop="description" class="description">>
-凭兴趣开发的简单drupal+SAE应用</p>
+<a href="http://bibles.sinaapp.com/" target="_blank">圣经在线</a>--凭兴趣开发的简单drupal+SAE应用</p>
 
 </div>
  <!-- Position #2 -->
 <div itemscope itemtype="http://schema.org/Organization">
 
 
-<p class="bus1">5：SAE应用开发：<span itemprop="name"><a href="http://livingwater.sinaapp.com/" target="_blank">SAE短信平台</a>（需登录）</span></p>
+<p class="bus1">6：<span itemprop="name"><img src="https://drupal.org/files/images/mobile_0.png" alt="" style="height:30px;" /> SAE应用开发：<span itemprop="name">SAE短信平台（需登录）</span></p>
 
 
 <p itemprop="description" class="description" >>
-使用SAE 短信API，基于drupal开发批量发送短信平台。</p>
+<a href="http://livingwater.sinaapp.com/" target="_blank">SAE短信平台</a>使用SAE 短信API，基于drupal开发批量发送短信平台。</p>
 
 </div>
 
@@ -232,15 +232,20 @@ DruEdu - Next generation education solutions for schools based on Drupal. And I 
 <section id="publications">
 
 <div itemscope itemtype="http://schema.org/Article"><span itemprop="name">
+<img border="0"  height="45px" src="https://lucene.apache.org/images/solr.png" alt="Solr Logo">
 <img src="images/markdown.ico" alt="" />Markdown
 <img src="images/git.ico" alt="" />Git
 <img src="http://forum.ubuntu.org.cn/favicon.ico" alt="" />Ubuntu
-<img src="http://switchy.samabox.com/images/icon-128.png" alt="switchy" width="32px"/>switchy!
+<!-- <img src="http://switchy.samabox.com/images/icon-128.png" alt="switchy" width="32px"/>switchy! -->
 <img src="http://developer.android.com/assets/images/dac_logo.png" alt="" /><br/>
-<span id="sae"><a href="http://sae.sina.com.cn" target="_blank" alt="SAE">SAE</a></span>
+<!-- <span id="sae"><a href="http://sae.sina.com.cn" target="_blank" alt="SAE">SAE</a></span> -->
 <!-- <img src="http://bkarak.wizhut.com/blog/wp-content/uploads/2012/01/app_engine-64.png" alt="switchy"/> -->
 <img src="http://symfony.com/images/common/logo/logo_symfony_header.png" alt="switchy"/>
-<!-- <img src="http://www.phonegap.cn/wp-content/uploads/2011/07/phonegap2.png" alt="" /> -->
+<img src="https://www.cloudcms.com/gitana/images/tour/resources/phonegap-logo.png" alt="" width="50px"/>
+<img src="https://developers.google.com/_static/b8c8119cd7/images/developers-logo.svg" style="
+    height: 45px;
+">
+
 <p>
 <li itemprop="itemListElement">godaddy、bluehost、万网、西部数码等vps生产环境配置</li>
 <li itemprop="itemListElement">Win2003服务器维护配置</li>
@@ -265,7 +270,12 @@ background-position: 0 -60px;
 }
 .description{
 	font-size:13px;
+	text-indent: 2em;
 }
+.description a,.description a:visited{
+	text-decoration: none;
+}
+
 </style>
 
 </div>
@@ -299,12 +309,12 @@ background-position: 0 -60px;
 <!-- Begin Publications Section -->
 
 <!-- Begin Education Section -->
-<!-- <dl>
+<dl>
 <dt>早期项目:</dt>
 <dd>
 
-<section id="education">
-<div itemscope itemtype="http://schema.org/EducationalOrganization">
+<!-- <section id="education">
+<div itemscope itemtype="###">
 <pre>2011/06 -- 2011/07：丰田金融学院论坛基于drupal系统的定制开发
 2011/05 -- 2011/06：兴家牧业REP维护drupal菜单系统，中英文解决方案，文件丢失调试
 2011/04 -- 2011/05：丰田金融e-learning需求开发 基于moodle选课系统开发和日志系统扩展
@@ -316,10 +326,10 @@ background-position: 0 -60px;
 </pre>
 
 </div>
-</section>
+</section> -->
 
 </dd>
-</dl> -->
+</dl>
 <!-- End Education Section -->
 
 <div class="clear">&nbsp;</div>
@@ -329,7 +339,7 @@ background-position: 0 -60px;
 <dt>求职意向:</dt>
 <dd>
 
-<section class="summary">网站开发相关，主要是编写代码，产品或交互也行，喜欢用户交互与研究，不过经验不足，喜欢开放的环境，活跃的团队，期待月薪税后<b>8k～11k</b>，非诚勿扰。到岗时间，随时。
+<section class="summary">网站开发相关，主要是编写代码，产品或交互也行，喜欢用户交互与研究，喜欢开放的环境，活跃的团队。
 </section>
 
 </dd>
